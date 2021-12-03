78dc3454695a5ef053885c56d104e506661f7f22
Liam Ashkenazi <ashkenaziliam55@gmail.com>
2021-12-03 13:00:48 +0200

    Change building.elevators to dict of lists

--- classes/Building.py
+++ classes/Building.py
@@ -4,25 +4,25 @@ from Elevator_classes import FastElevator, StandartElevator, CargoElevator
 class Building:
     def __init__(self, n):
         self.n = n
-        self.elevators = [ # The building will automaticly have three elevators at least
-            FastElevator(1),
-            StandartElevator(2),
-            CargoElevator(3)
-        ]
+        self.elevators = { # The building will automaticly have three elevators at least
+            "1" : [FastElevator(1)],
+            "2" : [StandartElevator(2)],
+            "3" : [CargoElevator(3)]
+        }
         for id in range(random.randint(1,3)):
             randomElevator = random.randint(1,3)
             print(f"Id: {id+4}, Random: {randomElevator}")
             if randomElevator == 1:
-                self.elevators.append(FastElevator(id+4))
+                self.elevators["1"].append(FastElevator(id+4))
             if randomElevator == 2:
-                self.elevators.append(StandartElevator(id+4))
+                self.elevators["2"].append(StandartElevator(id+4))
             if randomElevator == 3:
-                self.elevators.append(CargoElevator(id+4))
+                self.elevators["3"].append(CargoElevator(id+4))
     def print(self):
         print(f"This building has {self.n} floors\nThe elevators are: ")
-        for ele in self.elevators:
-            print(f"\nElevator {ele.id}: {type(ele)}")
+        for type in self.elevators:
+            print(f"\nElevator type : {type}")
+            for ele in self.elevators[type]:
+                print(ele.id)
             
         
-building = Building(20)
-building.print()
